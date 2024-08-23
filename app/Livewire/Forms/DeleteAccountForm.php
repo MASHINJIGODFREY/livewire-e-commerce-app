9fff931612d5bbb6dfb2f2c021a32d02f7c4908a
MASHINJIGODFREY <gmashinji@gmail.com>
2024-08-23 12:17:48 +0300

    Added 'delete-account' form logic

---
+++ app/Livewire/Forms/DeleteAccountForm.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Livewire\Forms;
+
+use Livewire\Attributes\Validate;
+use Livewire\Form;
+use App\Models\User;
+
+class DeleteAccountForm extends Form
+{
+    public $password = '';
+ 
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function rules()
+    {
+        return ['password' => ['required', 'current_password']];
+    }
+
+    public function checkPassword()
+    {
+        $this->validate();
+    }
+    
+    public function destroy(array $payload)
+    {
+        $this->user->delete();
+    }
+}
